1e79dfa3ed3274af47ea8f8b4cb5e83274c17d1b
김민수 <36617103+min19828257@users.noreply.github.com>
2020-02-22 08:37:59 +0900

    Create 중복조합.cpp

---
+++ 조합/중복조합.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<vector>
+#include<stack>
+#include<algorithm>
+#include<vector>
+
+#define MAXN 100
+
+using namespace std;
+
+void print_arr(int set[], int set_size) {
+    for (int i = 0; i < set_size; i++)
+    {
+        cout << set[i] << " ";
+    }
+    cout << endl;
+}
+
+void comb(int set[], int set_size, int n, int k, int index) {
+    
+    int i;
+
+    if (k == 0) {
+        print_arr(set,set_size);
+    }
+    else if (index == n) {
+        return;
+    }
+    else {
+        set[set_size] = index;
+        comb(set, set_size+1,n, k-1, index);
+        comb(set,set_size, n, k, index + 1);
+    }
+}
+
+int main() {
+    
+    int set[MAXN], n, k;
+
+    printf("input n, k: ");
+    scanf_s("%d %d", &n, &k);
+    comb(set,0, n, k, 0);
+    return 1;
+}
